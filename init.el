4eb6bcffd2fe9eed80da5587557e0f78b65232f1
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2025-09-25 01:55:46 +0100

    nit

--- init.el
+++ init.el
@@ -103,7 +103,6 @@
 ;;   ;;        ("M-<f5>" . standard-themes-rotate))
 ;;   )
 
-
 (use-package mood-line
   :custom (mood-line-glyph-alist mood-line-glyphs-fira-code)
   :config (mood-line-mode))
